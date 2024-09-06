4d26c22d425b79156e4409b7b87fec98bd7e7e0d
[Cloud3] 전일우 <59927358+FFFace@users.noreply.github.com>
2024-09-06 14:30:01 +0900

    fix: axios 환경변수 수정

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: process.env.REQUEST_URI,
+    baseURL: process.env.REACT_APP_REQUEST_URI,
     withCredentials: true,
 });
