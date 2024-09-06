6a3c1d1d2386d07a19e127371454d1379b7140de
[Cloud3] 전일우 <59927358+FFFace@users.noreply.github.com>
2024-09-06 14:31:21 +0900

    fix: props OnClick 추가

--- src/component/ui/button/CustomButton.jsx
+++ src/component/ui/button/CustomButton.jsx
@@ -1,14 +1,15 @@
 import { ThemeProvider, Button, createTheme } from "@mui/material";
 
 
-const CustomButton = ({children}) => {
+const CustomButton = ({...props}) => {
   return (
     <Button size='large' 
+      onClick={props.onClick}
       sx={{
         color: 'var(--color4)',
         ":hover": {background: 'var(--color2)'}
         }}>
-        {children}
+        {props.children}
     </Button>
   );
 };
