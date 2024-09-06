ab6263f35dbbb3f35c9066265eb28b928dbbb9f5
[Cloud3] 전일우 <59927358+FFFace@users.noreply.github.com>
2024-09-06 18:20:30 +0900

    feat: CustomTypography 추가

---
+++ src/component/ui/typography/CustomTypography.jsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+
+
+
+const CustomTypography = ({...props}) =>{
+  return (
+    <Typography color='var(--color4)' {...props}>{props.children}</Typography>
+  )
+}
+
+
+export default CustomTypography;
