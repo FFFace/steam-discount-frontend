ce0bb2ea2cbb6c81f112d088e56b75d365f36486
[Cloud3] 전일우 <59927358+FFFace@users.noreply.github.com>
2024-09-06 14:32:08 +0900

    feat: discountList 추가 - 데이터 요청 추가

--- src/pages/discountList/DiscountList.jsx
+++ src/pages/discountList/DiscountList.jsx
@@ -1,1 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Contants from "../../component/Contants";
+import { Box, Typography } from "@mui/material";
+import { axiosInstance } from '../../utils/axios';
+
+const DiscountList = () => {
+
+  const [discountList, setDiscountList] = useState(null);
+
+  useEffect(()=>{
+    const getDiscountList = async () =>{
+      try{
+        const response = await axiosInstance.get('/discount-list');
+        setDiscountList(response.data);
+        console.log(discountList);
+      } catch (exception){
+        console.log(exception);
+      }
+    }
+    getDiscountList();
+  }, []);
+
+  return(
+    <Contants>
+      <Box sx={{backgroundColor: 'var(--color2)'}}>
+        <Box sx={{padding: '10px'}}>
+          <Typography variant='h5' color='var(--color4)'>
+            스팀 할인 목록 100
+          </Typography>
+        </Box>
+      </Box>
+    </Contants>
+  );
+};
+
+export default DiscountList;
