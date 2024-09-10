cfd92df82dc543de85f873409078e92972204da6
[Cloud3] 전일우 <59927358+FFFace@users.noreply.github.com>
2024-09-10 14:06:08 +0900

    fix: custom button 수정

--- src/component/ui/button/CustomButton.jsx
+++ src/component/ui/button/CustomButton.jsx
@@ -3,12 +3,12 @@ import { ThemeProvider, Button, createTheme } from "@mui/material";
 
 const CustomButton = ({...props}) => {
   return (
-    <Button size='large' 
-      onClick={props.onClick}
-      sx={{
-        color: 'var(--color4)',
-        ":hover": {background: 'var(--color2)'}
-        }}>
+    <Button size='large' onClick={props.onClick} sx={{
+        color: 'var(--color4)', 
+        ":hover": {
+          background: 'var(--color2)'
+          }
+        }} {...props}>
         {props.children}
     </Button>
   );
