26a133854db0fe8be99732baebb90a2c9f42e826
[Cloud3] 전일우 <59927358+FFFace@users.noreply.github.com>
2024-09-10 14:39:33 +0900

    feaqt: 회원가입 페이지

---
+++ src/pages/user/UserRegister.jsx
@@ -0,0 +1,21 @@
+import Contants from "../../component/Contants";
+import { CustomBox } from "../../component/ui/box/CustomBox";
+import CustomTypography from "../../component/ui/typography/CustomTypography";
+
+
+
+const UserRegister = () => {
+  return (
+    <Contants>
+      <CustomBox>
+        <CustomTypography>
+          회원 가입
+        </CustomTypography>
+      </CustomBox>
+    </Contants>
+  );
+};
+
+
+
+export default UserRegister;
