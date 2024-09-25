0ec12579f9407e39a1dbe40bbc097bca61d63285
[Cloud3] 전일우 <59927358+FFFace@users.noreply.github.com>
2024-09-25 20:24:42 +0900

    feat: 헤더 추가

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -3,4 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_REQUEST_URI,
     withCredentials: true,
+    headers: {
+        "Authorization": localStorage.getItem('token')
+    }
 });
