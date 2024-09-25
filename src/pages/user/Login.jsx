8a378728d2adc0170f4eb573cac68292fcc12c72
[Cloud3] 전일우 <59927358+FFFace@users.noreply.github.com>
2024-09-25 20:24:20 +0900

    feat: recoil 로그인 전역상태 추가

--- src/pages/user/Login.jsx
+++ src/pages/user/Login.jsx
@@ -10,13 +10,19 @@ import { axiosInstance } from "../../utils/axios";
 import { setLoginState, saveAccessToken } from "../../utils/storage";
 import { CustomDialog, CustomDialogContent, CustomDialogTitle } from "../../component/ui/dialog/CustomDialog";
 import Loading from "../../component/ui/loading/Loading";
+import ErrorComponent, { LoginFailed, NeedDuplicateEmail } from "../../component/ui/error/error";
+import { useRecoilState } from "recoil";
+import { userState } from "../../utils/atom";
 
 const Login = () => {
 
   const navigate = useNavigate();
 
-  const [isLogin, setIsLogin] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [errorState, setErrorState] = useState({
+    loginFailed: false,
+    needDuplicateEmail: false,
+  })
 
   const [info, setInfo] = useState({
     email: '',
@@ -29,6 +35,8 @@ const Login = () => {
       [e.target.name]: e.target.value
     });
   }
+
+  const [state, setState] = useRecoilState(userState);
 
   const onClickRegister = () => {
     navigate('/user-register');
@@ -45,28 +53,39 @@ const Login = () => {
   const onClickLoginButton = async () => {
     setLoading(true);
     try{
-      const response = await axiosInstance.post(`/users/login`, {
+      const response = await axiosInstance.post(`/login`, {
         email: info.email,
         password: info.password
       });
 
       saveAccessToken(response.headers['authorization']);
-      setIsLogin(true);
+      setState({
+        ...state,
+        isLoggedIn: true
+      })
 
     } catch(exception){
-      console.log(exception);
+      console.log(exception.response);
+
+      if(exception.response.status === 400){
+        setState({
+          ...state,
+          isLoggedIn: false
+        })
+      } else if(exception.response.status === 403){
+        setErrorState({
+          ...errorState,
+          needDuplicateEmail: true
+        })
+      }
     }
 
     setLoading(false);
   }
 
-  useEffect(() => {
-    setLoginState(true);
-  }, [isLogin])
-
   const LoginSuccess = () => {
     return(
-      <CustomDialog open={isLogin}>
+      <CustomDialog open={state.isLoggedIn}>
         <CustomDialogTitle>로그인 성공</CustomDialogTitle>
         <CustomDialogContent>
           로그인되었습니다.<br/>
@@ -79,13 +98,32 @@ const Login = () => {
     );
   }
 
+  const loginFailedDialogAccept = () => {
+    setErrorState({
+      ...errorState,
+      loginFailed: false
+    });
+  }
+
+  const needDuplicateDialogAccept = () => {
+    setErrorState({
+      ...errorState,
+      needDuplicateEmail: false
+    });
+
+    navigate('/email-validation', {
+      state: {
+        email: info.email
+      }
+    });
+  }
+
   const onClickLoginSuccess = () => {
     navigate('/');
   }
 
   return (
     <Contants>
-      <Loading open={loading} />
       <CustomBox>
         <CustomTypography variant='h5'>
           로그인
@@ -102,7 +140,7 @@ const Login = () => {
         <CustomTypography sx={{margin: '5px'}} >비밀번호</CustomTypography>
         <CustomTextField name='password' onChange={onChangeInfo} value={info.password}/>
 
-        <Box sx={{margin: '25px 0px auto'}} onClick={onClickLoginButton}>
+        <Box sx={{margin: '25px 0px auto'}}>
           <ButtonComponent onClick={onClickLoginButton}>
             로그인
           </ButtonComponent>
@@ -112,6 +150,9 @@ const Login = () => {
         </Box>
       </CustomBox>
       <LoginSuccess/>
+      <LoginFailed loginFailedOpen={errorState.loginFailed} loginFailedDialogAccept={loginFailedDialogAccept}/>
+      <NeedDuplicateEmail needDuplicateOpen={errorState.needDuplicateEmail} needDuplicateDialogAccept={needDuplicateDialogAccept}/>
+      <Loading open={loading} />
     </Contants>
   );
 };
