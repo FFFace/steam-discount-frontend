dbc82c252934e95de2ca4b6017a6f8a1d8c65ab1
FFFace <59927358+FFFace@users.noreply.github.com>
2024-10-07 16:35:39 +0900

    feat: 쿠키 삭제 및 로컬 저장소 데이터 삭제 추가;

--- src/utils/storage.jsx
+++ src/utils/storage.jsx
@@ -1,9 +1,20 @@
+import cookie from 'react-cookies'
 
 
 
 
-const STORAGE_TOKEN = 'token';
+const STORAGE_ACCESS_TOKEN = 'token';
+const STORAGE_LOGGED = 'logged';
+const STORAGE_REFRESH_TOKEN = 'refreshToken'
 
 export const saveAccessToken = (token) => {
-  localStorage.setItem(STORAGE_TOKEN, token);
+  localStorage.setItem(STORAGE_ACCESS_TOKEN, token);
+}
+
+export const removeAccessToken = () => {
+  localStorage.removeItem(STORAGE_ACCESS_TOKEN);
+}
+
+export const removeRefreshToken = () => {
+  cookie.remove(STORAGE_REFRESH_TOKEN);
 }
