6115e07f83e0508db9c036b7bd775a392558c948
FFFace <59927358+FFFace@users.noreply.github.com>
2024-10-28 14:43:16 +0900

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Header from './component/Header';
-import Contents from './component/Contents';
-import { Typography } from '@mui/material';
-import { useRecoilState } from 'recoil';
-import { userState } from './utils/atom';
-import { axiosInstance } from './utils/axios';
-import { useEffect } from 'react';
-import { saveAccessToken } from './utils/storage';
+import Main from './pages/main/Main';
 
 
 function App() {
 
   return (
-    <Contents>  
-      <Typography>
-        MUI Body
-      </Typography>  
-    </Contents>
+    <Main/>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Header from './component/Header';
 import DiscountList from './pages/discountList/DiscountList';
-import Login from './pages/user/Login'
+import Login from './pages/user/Login';
 import { createTheme, ThemeProvider } from '@mui/material';
 import UserRegister from './pages/user/UserRegister';
 import EmailValidation from './pages/user/EmailValidation';
@@ -22,59 +22,28 @@ const theme = createTheme({
   typography: {
     fontFamily: '"Noto Sans KR", "Roboto","Helvetica","Arial",sans-serif'
   }
-})
-
-const bodyRouter = createBrowserRouter([
-  {
-    path: '/',
-    element: <Main/>
-  },
-  {
-    path: '/discount-list',
-    element: <DiscountList/>
-  },
-  {
-    path: '/login',
-    element: <Login/>
-  },
-  {
-    path: '/user-register',
-    element: <UserRegister/>
-  },
-  {
-    path: '/email-validation',
-    element: <EmailValidation/>
-  },
-  {
-    path: '/board',
-    element: <Board/>
-  },
-  {
-    path: '/post',
-    element: <Post/>
-  },
-  {
-    path: '/write-post',
-    element: <WritePost/>
-  },
-  {
-    path: '/my-page',
-    element: <MyPage/>
-  },
-  {
-    path: '/writed-post-list',
-    element: <WritedPostList/>
-  }
-]);
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThemeProvider theme={theme}>
     <RecoilRoot>
-      <RouterProvider router={bodyRouter}>
-      </RouterProvider>
-    </RecoilRoot>    
-  </ThemeProvider>  
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/discount-list" element={<DiscountList />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/user-register" element={<UserRegister />} />
+          <Route path="/email-validation" element={<EmailValidation />} />
+          <Route path="/board" element={<Board />} />
+          <Route path="/post" element={<Post />} />
+          <Route path="/write-post" element={<WritePost />} />
+          <Route path="/my-page" element={<MyPage />} />
+          <Route path="/writed-post-list" element={<WritedPostList />} />
+        </Routes>
+      </BrowserRouter>
+    </RecoilRoot>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
