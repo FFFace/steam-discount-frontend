6b4c3363b82a0450c02d1836c4c2de4c4121f9a9
FFFace <59927358+FFFace@users.noreply.github.com>
2024-11-26 15:11:46 +0900

    fix: 기타 정보 추가

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,6 +1,7 @@
 import { Box, Container } from '@mui/material';
 import CustomTypography from './ui/typography/CustomTypography';
 import { CustomBox } from './ui/box/CustomBox';
+import { Link } from 'react-router-dom';
 
 
 
@@ -14,14 +15,27 @@ const Footer = () =>{
         <CustomTypography sx={{fontSize: '14px', color: 'var(--color3)'}}>
           email: ilwoo6422@naver.com
         </CustomTypography>
-        <CustomTypography>
-          notion: https://stellar-anger-633.notion.site/Steam-Discount-1352fb0804b5801b9aa2e354e25cd8a4
+        <a href='https://stellar-anger-633.notion.site/Steam-Discount-1352fb0804b5801b9aa2e354e25cd8a4' target='_blank'>
+          <CustomTypography sx={{fontSize: '14px', color: 'var(--color3)'}}>
+            notion: https://stellar-anger-633.notion.site/Steam-Discount-1352fb0804b5801b9aa2e354e25cd8a4
+          </CustomTypography>
+        </a>
+
+        <CustomTypography sx={{fontSize: '14px', color: 'var(--color3)'}}>
+          github
         </CustomTypography>
-        <CustomTypography>
-          github<br/>
-          back-end: https://github.com/FFFace/steam-discount
-          front-end: https://github.com/FFFace/steam-discount-frontend
-        </CustomTypography>
+
+        <a href='https://github.com/FFFace/steam-discount' target='_blank'>
+          <CustomTypography sx={{fontSize: '14px', color: 'var(--color3)'}}>
+            back-end: https://github.com/FFFace/steam-discount
+          </CustomTypography>
+        </a>        
+        
+        <a href='https://github.com/FFFace/steam-discount-frontend' target='_blank'>
+          <CustomTypography sx={{fontSize: '14px', color: 'var(--color3)'}}>
+            front-end: https://github.com/FFFace/steam-discount-frontend
+          </CustomTypography>       
+        </a>
       </Box>      
     </CustomBox>         
   )
